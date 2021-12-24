04f6e2178787e5c4f759114e4bfdd920d74cf976
myatmaung123 <96323055+myatmaung123@users.noreply.github.com>
2021-12-24 15:00:14 +0630

    ok

--- boostrap project/codeing chialling/guse.js
+++
@@ -1,9 +0,0 @@
-//add an aerry list 
-const guesslist = ["luffy", "zoro", "nami", "usopp", "sanji", "chopper", "vivi"];
-const userInput =prompt("What is you name ?");
-
-if(guesslist.includes(userInput)){
-    alert(`Welcome to our tame, ${userInput}`);
-}else{
-    alert("You are not on the list...");
-}
--- boostrap project/codeing chialling/treatlunch.js
+++
@@ -1,8 +0,0 @@
-function treatlunch(){
-const crew = ["luffy", "zoro", "chopper", "usoop", "sanji", "nami", "robin", "franky"];
-
-const totalCrew = crew.length;
-const random =Math.floor(Math.random() * totalCrew)// 0 - 0.9 * 9
-const treat = crew[random];
-return `${treat} will by lunch for all of us`
-}
